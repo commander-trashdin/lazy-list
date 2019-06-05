bedde2fec673eb96226ec8c34e9f1d87d95203dc
Andrey Piskunov <Sokolov11112008@yandex.ru>
2019-06-06 02:27:16 +0300

    Made everything lazy

--- laziness.lisp
+++ laziness.lisp
@@ -52,7 +52,7 @@
 
 (defun %lazy-list (fst foo)
   (lazy-cons fst (%lazy-list (funcall foo fst) foo)))
-;;this probably will be useful
+
 
 (defun lazy-list (fst &key (snd (1+ fst)) lst)
   (let ((step (- snd fst)))
@@ -60,12 +60,12 @@
         (if (> fst lst)  ;;maybe better in terms of take n?
             nil
             (lazy-cons fst (lazy-list (+ fst step) :snd (+ snd step) :lst lst)))
-        (lazy-cons fst (lazy-list (+ fst step) :snd (+ snd step))))))
+        (%lazy-list fst (lambda (x) (+ step x))))))
 
 
 ;;; ----------------------------------------------------
 
-(defun print-lazy-list (lazylist &optional (stream t))
+(defun coerce-to-list (lazylist &optional (stream t))
     (labels ((collect-lazy-list (lazylist acc)
                 (if lazylist
                     (collect-lazy-list (lazy-cdr lazylist) (cons (lazy-car lazylist) acc))
@@ -84,6 +84,18 @@
       initial-val)
     (foldl foo (lazy-cdr lazylist) :initial-val (lazy-car lazylist))))
 
+(defun findl (pred lazylist)
+  (if (funcall pred (lazy-car lazylist))
+      (lazy-car lazylist)
+      (findl pred (lazy-cdr lazylist))))
+
+
+(defun scanl (foo lazylist &key initial-val)
+  (if initial-val
+    (lazy-cons initial-val (scanl foo (lazy-cdr lazylist) :initial-val (funcall foo initial-val (lazy-car lazylist))))
+    (scanl foo (lazy-cdr lazylist) :initial-val (lazy-car lazylist))))
+
+
 ;;; ----------------------------------------------------
 
 (defun lazy-map (foo &rest lazylists)
@@ -91,19 +103,28 @@
     (lazy-cons (apply foo (mapcar #'lazy-car lazylists))
                (apply #'lazy-map foo (mapcar #'lazy-cdr lazylists)))))
 
+(defun replicate (obj &optional (times nil))
+  (if times
+    (if (plusp times)
+        (lazy-cons obj (replicate obj (1- times)))
+        nil)
+    (%lazy-list obj (lambda (obj) obj))))
+
+
 ;;; ----------------------------------------------------
 
 (defun take (n lazylist)
-  (labels ((%take (n lazylist acc)
-              (if (plusp n)
-                  (%take (1- n) (lazy-cdr lazylist) (cons (lazy-car lazylist) acc))
-                  acc)))
-    (reverse (%take n lazylist (list)))))
+  (if (plusp n)
+      (lazy-cons (lazy-car lazylist) (take (1- n) (lazy-cdr lazylist)))
+      nil))
 
 (defun take-while (pred lazylist)
-  (labels ((%take-while (pred lazylist acc)
-              (let ((candidate (lazy-car lazylist)))
-                (if (funcall pred candidate)
-                    (%take-while pred (lazy-cdr lazylist) (cons candidate acc))
-                    acc))))
-    (reverse (%take-while pred lazylist (list)))))
+  (let ((candidate (lazy-car lazylist)))
+    (if (funcall pred candidate)
+        (lazy-cons candidate (take-while pred (lazy-cdr lazylist)))
+        nil)))
+
+(defun lazy-nth (n lazylist)
+  (if (plusp n)
+      (lazy-nth (1- n) (lazy-cdr lazylist))
+      (lazy-car lazylist)))
