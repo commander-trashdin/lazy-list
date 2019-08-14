1d4467f3666b3d8cb46c6170731ee52e2120539c
Andrew <aun.sokolov@gmail.com>
2019-08-15 02:00:39 +0300

    Export works

--- package.lisp
+++ package.lisp
@@ -1,4 +1,18 @@
 ;;;; package.lisp
 
 (defpackage #:laziness
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:emptyp
+           #:lazy-car #:lazy-cdr
+           #:lazy-cons
+           #:lazy-list
+           #:coerce-to-list
+           #:foldl #:findl #:scanl
+           #:lazy-map
+           #:replicate
+           #:take
+           #:take-while
+           #:lazy-nth
+           #:filter
+           #:concat
+           #:%cartesian))
